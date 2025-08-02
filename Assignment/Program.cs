987f2d3653a97282d3c6e0c9fa69367cdbaecbe3
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-02 18:59:17 +0300

    Question 02

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,13 +5,32 @@
         static void Main(string[] args)
         {
             #region Question 01
-          
-            IShape circle = new Circle(5);
-            circle.DisplayShapeInfo();
 
-            IShape rectangle = new Rectangle(4, 6);
-            rectangle.DisplayShapeInfo();
-           
+            //IShape circle = new Circle(5);
+            //circle.DisplayShapeInfo();
+
+            //IShape rectangle = new Rectangle(4, 6);
+            //rectangle.DisplayShapeInfo();
+
+            #endregion
+
+            #region Question 02
+
+            IAuthenticationService authService = new BasicAuthenticationService();
+            bool isAuthenticated = authService.AuthenticateUser("admin", "password123");
+            bool isAuthorized = authService.AuthorizeUser("admin", "Admin");
+
+            Console.WriteLine($"Authentication: {isAuthenticated}");
+            Console.WriteLine($"Authorization: {isAuthorized}");
+            Console.WriteLine();
+
+            IAuthenticationService authService02 = new BasicAuthenticationService();
+            bool isAuthenticated02 = authService02.AuthenticateUser("admin", "password");
+            bool isAuthorized02 = authService02.AuthorizeUser("user", "User");
+
+            Console.WriteLine($"Authentication: {isAuthenticated02}");
+            Console.WriteLine($"Authorization: {isAuthorized02}");
+            Console.WriteLine();
             #endregion
         }
     }
