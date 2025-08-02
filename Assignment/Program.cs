67c817cec3416ba0d961e6886664fc20befea500
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-02 19:03:01 +0300

    Question 03

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -16,21 +16,32 @@
 
             #region Question 02
 
-            IAuthenticationService authService = new BasicAuthenticationService();
-            bool isAuthenticated = authService.AuthenticateUser("admin", "password123");
-            bool isAuthorized = authService.AuthorizeUser("admin", "Admin");
+            //IAuthenticationService authService = new BasicAuthenticationService();
+            //bool isAuthenticated = authService.AuthenticateUser("admin", "password123");
+            //bool isAuthorized = authService.AuthorizeUser("admin", "Admin");
 
-            Console.WriteLine($"Authentication: {isAuthenticated}");
-            Console.WriteLine($"Authorization: {isAuthorized}");
-            Console.WriteLine();
+            //Console.WriteLine($"Authentication: {isAuthenticated}");
+            //Console.WriteLine($"Authorization: {isAuthorized}");
+            //Console.WriteLine();
 
-            IAuthenticationService authService02 = new BasicAuthenticationService();
-            bool isAuthenticated02 = authService02.AuthenticateUser("admin", "password");
-            bool isAuthorized02 = authService02.AuthorizeUser("user", "User");
+            //IAuthenticationService authService02 = new BasicAuthenticationService();
+            //bool isAuthenticated02 = authService02.AuthenticateUser("admin", "password");
+            //bool isAuthorized02 = authService02.AuthorizeUser("user", "User");
 
-            Console.WriteLine($"Authentication: {isAuthenticated02}");
-            Console.WriteLine($"Authorization: {isAuthorized02}");
-            Console.WriteLine();
+            //Console.WriteLine($"Authentication: {isAuthenticated02}");
+            //Console.WriteLine($"Authorization: {isAuthorized02}");
+
+            #endregion
+
+            #region Question 03
+           
+            INotificationService emailService = new EmailNotificationService();
+            INotificationService smsService = new SmsNotificationService();
+            INotificationService pushService = new PushNotificationService();
+
+            emailService.SendNotification("user@example.com", "Hello via Email!");
+            smsService.SendNotification("+1234567890", "Hello via SMS!");
+            pushService.SendNotification("device123", "Hello via Push Notification!");
             #endregion
         }
     }
