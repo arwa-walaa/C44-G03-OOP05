48b76487ae9bc24ce4d9a1d9671d873a28ed13d8
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-02 16:00:08 +0300

    Binding

---
+++ OOPSession5/Binding/TypeA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSession5.Binding
+{
+    internal class TypeA
+    {
+        public int A { get; set; }
+        public TypeA(int a)
+        {
+            A = a;
+        }
+        public void funct01()
+        {
+            Console.WriteLine("Parent Class");
+        }
+
+        public virtual void funct02()
+        {
+            Console.WriteLine($"TypeA A: {A}");
+        }
+
+    }
+}
--- OOPSession5/Program.cs
+++ OOPSession5/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace OOPSession5
+﻿using OOPSession5.Binding;
+
+namespace OOPSession5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Binding
+            TypeA type = new TypeB(10,5);
+            type.A = 20;
+            //type.B = 30;
+
+            type.funct01(); // Calls TypeA's funct01 due to 'new' keyword in TypeB
+            type.funct02(); // Calls TypeB's funct02 due to 'override' keyword in TypeB
+
+            #endregion
         }
     }
 }
