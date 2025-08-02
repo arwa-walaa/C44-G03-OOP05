a6555dcdccdcc391ca31700a3bdb8d75d7650157
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-02 16:20:45 +0300

    Binding Example

---
+++ OOPSession5/Binding/FullTimeEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSession5.Binding
+{
+    internal class FullTimeEmployee : Employee
+
+    {
+        public decimal salary { get; set; }
+
+        public override void func01()
+        {
+            Console.WriteLine("Hello full time Employee");
+        }
+        public override void func02()
+        {
+            Console.WriteLine($"Emp ID: {id} | Name: {name} | Age: {age} | salary: {salary}");
+
+        }
+    }
+}
--- OOPSession5/Program.cs
+++ OOPSession5/Program.cs
@@ -4,6 +4,36 @@ namespace OOPSession5
 {
     internal class Program
     {
+        #region Binding usege
+        //public static void ProcessEmp(FullTimeEmployee emp)
+        //{
+        //    if (emp is not null)
+        //    {
+        //        emp.func01();
+        //        emp.func02();
+        //    }
+
+        //}
+        //public static void ProcessEmp(PartTimeEmployee emp)
+        //{
+        //    if (emp is not null)
+        //    {
+        //        emp.func01();
+        //        emp.func02();
+        //    }
+
+        //}
+        public static void ProcessEmp(Employee emp)
+        {
+            if (emp is not null)
+            {
+                emp.func01();
+                emp.func02();
+            }
+
+        }
+        #endregion
+
         static void Main(string[] args)
         {
             #region Binding
@@ -11,10 +41,20 @@ namespace OOPSession5
             type.A = 20;
             //type.B = 30;
 
-            type.funct01(); // Calls TypeA's funct01 due to 'new' keyword in TypeB
-            type.funct02(); // Calls TypeB's funct02 due to 'override' keyword in TypeB
+            //type.funct01(); // Calls TypeA's funct01 due to 'new' keyword in TypeB
+            //type.funct02(); // Calls TypeB's funct02 due to 'override' keyword in TypeB
 
             #endregion
+
+            #region Binding Usage
+            Employee FullTimeEmp = new FullTimeEmployee();
+            Employee PartTimeEmp = new PartTimeEmployee();
+            ProcessEmp(FullTimeEmp);
+            ProcessEmp(PartTimeEmp);
+
+            #endregion
+
+
         }
     }
 }
