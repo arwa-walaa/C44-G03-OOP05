4b38ce905a451dfb705c07c60a224ecb1a9bfdac
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-08-02 15:21:57 +0300

    Add project files.

---
+++ OOPSession5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPSession5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
